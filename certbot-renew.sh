2a31fe9514e53f6fc4bb00e0e0c33cb626b92e93
Quyet Nguyen <quyetnd112@gmail.com>
2018-08-01 15:24:54 +0700

    Update renew script

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,6 +1,6 @@
-docker run -it --rm \
-    -v serverinit_certbot-conf: /etc/letsencrypt \
-    -v serverinit_certbot-certs: /var/lib/letsencrypt \
-    -v serverinit_nginx-webroot: /usr/share/nginx/html \
+docker run -i --rm \
+    -v serverinit_certbot-conf:/etc/letsencrypt \
+    -v serverinit_certbot-certs:/var/lib/letsencrypt \
+    -v serverinit_nginx-webroot:/usr/share/nginx/html \
     certbot/certbot renew
 docker exec nginx nginx -s reload
